0d2fc218d4148b01cf9f87ce755ac1d0c08222ed
Pat Nakajima <patnakajima@gmail.com>
2008-10-31 22:16:12 -0400

    added extensions to ghoulish modules

--- lib/got_yo_back/callbacker.rb
+++ lib/got_yo_back/callbacker.rb
@@ -8,24 +8,31 @@ module GotYoBack
     end
     
     def extend_klass
-      klass.class_eval do
-        @@pristine_cache = { }
-        
-        def pristine(method_id, *args, &block)
-          if method = @@pristine_cache[method_id]
-            method.bind(self).call *args, &block
-          else
-            raise NoMethodError, "No method named #{method_id.inspect}"
-          end
-        end
-      end
+      klass.extend(ClassMethods)
+      klass.send :include, InstanceMethods
     end
     
     def before(method_id, &block)
       klass.class_eval do
-        @@pristine_cache[method_id] ||= instance_method(method_id)
+        pristine_cache[method_id] ||= instance_method(method_id)
         define_method(method_id) do |*args|
           pristine(method_id, *args) if instance_eval(&block)
+        end
+      end
+    end
+    
+    module ClassMethods
+      def pristine_cache
+        @pristine_cache ||= Hash.new
+      end
+    end
+    
+    module InstanceMethods
+      def pristine(method_id, *args, &block)
+        if method = self.class.pristine_cache[method_id]
+          method.bind(self).call *args, &block
+        else
+          raise NoMethodError, "No method named #{method_id.inspect}"
         end
       end
     end
